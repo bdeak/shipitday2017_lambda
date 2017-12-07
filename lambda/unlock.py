0174c081a3e3be409bb59fd039af73635a5910e5
Balint Deak <balint.deak@porsche.de>
2017-12-07 20:08:20 +0000

    working unlock endpoint

--- lambda/unlock.py
+++ lambda/unlock.py
@@ -1,22 +1,52 @@
 import json
 from boto3 import resource
 from boto3.dynamodb.conditions import Key
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def my_logging_handler(event, context):
+    logger.info('got event{}'.format(event))
+
+def check_access(data, imei, phone_token):
+    for element in data:
+        if element['imei'] == imei and element['phone_token'] == phone_token:
+            return True
+    return False
+
 
 def lambda_handler(event, context):
     # list all entries from the db
     table_name = "shipitday2017_face_the_performance_data"
     dynamodb_resource = resource('dynamodb')
     table = get_table(dynamodb_resource, table_name)
-    # get all items, while filtering out some columns
+    logger.info('got event{}'.format(event))
+    request_data = json.loads(event['body'])
+    imei = request_data['imei']
+    phone_token = request_data['phone_token']
+    # get data from db and filter it
     entries = scan_table(table)['Items']
     entries_data = [entries[0], entries[-1]]
-    #return entries_data
-    masked_fields = [ 'token' ]
     result_list = []
     for data_group in entries_data:
-        entries_filtered = {k: v for k, v in data_group.items() if k not in masked_fields}
+        entries_filtered = {k: v for k, v in data_group.items() if k in ['imei', 'phone_token']}
         result_list.append(entries_filtered)
-    return http_response(200, result_list)
+
+    # look for an element where imei == imei and token == token
+    if check_access(result_list, imei, phone_token) == False:
+        return http_response(403, { "success": "false", "message": "Access denied" })
+
+
+    # update need_to_be_opened
+    update_opened_status(table, imei, True)
+
+    return http_response(200, { "success": "true", "message": "Command accepted." })
+
+def update_opened_status(table, imei, state):
+        table.update_item(Key={'imei':imei},
+            UpdateExpression="SET need_to_be_opened = :need_to_be_opened",
+            ExpressionAttributeValues={':need_to_be_opened': state})
 
 def get_table(dynamodb_resource, table_name):
     return dynamodb_resource.Table(table_name)
